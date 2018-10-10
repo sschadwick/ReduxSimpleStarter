ce74a6d158eb5b402cb2fb72ef7b2cc75aee59a8
abpmad <donghe1216@gmail.com>
2018-10-10 08:23:33 -0700

    10.09

---
+++ src/components/search_bar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component{
+
+   render() {
+      return <input onChange={(event) => console.log(event.target.value)} />;
+   }
+
+   // onInputChange(event) {
+   //    console.log(event.target.value);
+   // }
+}
+
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+// const API_KEY = '';
 
 // Create a new component. This component should produce
 // some html
 
-const App = function(){
-  return <div>Hi!</div>;
+const App = () => {
+   return (
+      <div>
+         <SearchBar />
+      </div>
+   );
 }
 // Take this compoent's generated HTML and put ti on the page(in the DOM)
 
-ReactDOM.render(App);
+ReactDOM.render(<App />, document.querySelector(".container"));
