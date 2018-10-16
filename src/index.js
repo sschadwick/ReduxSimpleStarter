4aa00862e8915934874ea4d1af0d9b4bb29489b2
abpmad <donghe1216@gmail.com>
2018-10-16 00:43:28 -0700

    10.15

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 // const API_KEY = '';
 
-// Create a new component. This component should produce
-// some html
 
-const App = () => {
-   return (
-      <div>
-         <SearchBar />
-      </div>
-   );
+class App extends Component{
+   constructor(props){
+      super(props);
+
+      this.state = { videos: [] };
+
+      YTSearch({key: API_KEY, term: 'chrisFix'}, (videos)=>{
+         // this.setState( {videos: videos}); //key and property are the same.
+         this.setState( { videos } );
+      });
+   }
+   render(){
+      return (
+         <div>
+            <SearchBar />
+            <VideoList videos={this.state.videos} />
+         </div>
+      )
+   }
 }
-// Take this compoent's generated HTML and put ti on the page(in the DOM)
-
 ReactDOM.render(<App />, document.querySelector(".container"));
